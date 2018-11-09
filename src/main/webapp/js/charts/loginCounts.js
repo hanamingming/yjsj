2ad436f22663604100f6f1e076b5e3509a218830
wang0812 <804632308@qq.com>
2018-11-09 18:48:39 +0800

    登陆次数统计

---
+++ src/main/webapp/js/charts/loginCounts.js
@@ -0,0 +1,24 @@
+
+  $(function () {
+    "use strict";
+    $.get("/yjsj/login/selectCounts.do",function(jsonObj){
+    	if(jsonObj.status==0){
+    		var list = jsonObj.data;
+    		
+    		  //BAR CHART
+    	    var bar = new Morris.Bar({
+    	      element: 'bar-chart',
+    	      resize: true,
+    	      data:list,
+    	      barColors: ['#00a65a', '#f56954'],
+    	      xkey: 'year',
+    	      ykeys: ['con1', 'con2'],
+    	      labels: ['con1', 'con2'],
+    	      hideHover: 'auto'
+    	    });
+    	
+    	}
+   },'json');
+   
+  
+  });
